d5dea9a4df0586d33a15c984860f9492f83c6a95
Gabriel Araujo De Souza <gabriel_2016@A303-16L.imd.local>
2019-05-23 11:26:34 -0300

    include the reader od links

--- reactive-TFidF/src/main/java/bigdata/reactive/actors/input/ReadLinkActor.java
+++ reactive-TFidF/src/main/java/bigdata/reactive/actors/input/ReadLinkActor.java
@@ -1,13 +1,64 @@
 package bigdata.reactive.actors.input;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import akka.actor.AbstractActor;
+import akka.actor.Props;
 
 public class ReadLinkActor extends AbstractActor {
 
 	@Override
 	public Receive createReceive() {
-		// TODO Auto-generated method stub
-		return null;
+		return receiveBuilder()
+				.match(String.class , msg -> {
+					getSender().tell(readUrls(msg), getSelf());
+				}).build();
+	}
+	
+	/**
+	 * read the file and separate all urls for read.
+	 * @param m  the url to file
+	 * @return all urls in the file
+	 */
+	private List<String> readUrls ( String m ){
+		
+		// create the list of urls
+		ArrayList<String> urls = new ArrayList<String>();
+		
+		try {
+			
+			// open file
+			FileReader file = new FileReader( m );
+			BufferedReader readFile = new BufferedReader(file);
+			String line = readFile.readLine();
+			
+			// read all lines (urls)
+			while (line != null) {
+				line.trim();
+				urls.add(line);
+				line = readFile.readLine();
+			}
+			
+			// close file
+			file.close();
+		} catch (IOException e1) {
+			System.err.println("could not open file " + m 
+					+ " " + e1.getMessage());
+		}
+		
+		return urls;
+	}
+	
+	/**
+	 * method to return a Props
+	 * @return return a this class Props 
+	 */
+	public static Props props () {
+		return Props.create(ReadLinkActor.class);
 	}
 
 }

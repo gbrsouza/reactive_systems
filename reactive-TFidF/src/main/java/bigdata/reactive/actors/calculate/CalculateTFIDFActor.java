933995013d7ec0e386e3b84c600ccd1717365cb0
GabrielArSouza <gabriel_feg@hotmail.com>
2019-06-10 22:19:21 -0300

    create TFIDF actors

---
+++ reactive-TFidF/src/main/java/bigdata/reactive/actors/calculate/CalculateTFIDFActor.java
@@ -0,0 +1,19 @@
+package bigdata.reactive.actors.calculate;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+
+public class CalculateTFIDFActor extends AbstractActor {
+
+	@Override
+	public Receive createReceive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Props props () {
+		return Props.create(CalculateTFIDFActor.class);
+	}
+	
+	
+}
